38b0a277d18ef63095946d9e91b885eeb7a44e27
Nikolaj Volosnikoff <volosnikovkolia@gmail.com>
2020-01-06 19:33:27 +0200

    Working on flask powered web interface.

--- app.py
+++ app.py
@@ -14,21 +14,41 @@ def articles_all():
     articles = []
     for i, (title, href, type, text) in dataset.iterrows():
         articles.append({
-            'pk': i,
-            'title': title,
+            'id': i,
+            'header': title,
             'type': type,
             'href': href,
+            'description': ' '.join(text.split('.')[:3]).replace('\n', ''),
             'text': text
         })
 
-    return render_template('articles.html', articles=articles)
+    return render_template('articles.html', articles=articles, articles_count=len(articles), category_name='Все Статьи')
 
-@app.route('/articles/<pk>/')
-def articles_single(pk=None):
+@app.route('/articles/cat/<category_name>')
+def articles_category(category_name=None):
+    dataset = pd.read_csv('dataset.csv')
+
+    articles = []
+    for i, (title, href, type, text) in dataset.iterrows():
+        if type == category_name.lower():
+            articles.append({
+                'id': i,
+                'header': title,
+                'type': type,
+                'href': href,
+                'description': text[:250],
+                'text': text
+            })
+
+    return render_template('articles.html', articles=articles, articles_count=len(articles), category_name=category_name.capitalize())
+
+@app.route('/articles/<id>/')
+def articles_single(id=None):
     dataset = pd.read_csv('dataset.csv')
     article = {
-        'title': dataset.iloc[int(pk)].title,
-        'text': dataset.iloc[int(pk)].text,
+        'id': id,
+        'title': dataset.iloc[int(id)].title,
+        'text': dataset.iloc[int(id)].text,
     }
 
     return render_template('article.html', article=article)
