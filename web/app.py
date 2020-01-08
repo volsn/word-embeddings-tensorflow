c98bd4bb5af76245e7cea8b471db7e39d0f7021c
Nikolaj Volosnikoff <volosnikovkolia@gmail.com>
2020-01-08 09:52:56 +0200

    orginized files.

--- web/app.py
+++ web/app.py
@@ -7,17 +7,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-
-'''
-from tensorflow.keras.preprocessing.text import Tokenizer, one_hot
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras import layers, Sequential
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.initializers import Constant
-'''
 
 app = Flask(__name__)
 
